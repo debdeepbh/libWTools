918052d8fddc9a3192d1036c481b114dd645c302
Debdeep <6978309+debdeepbh@users.noreply.github.com>
2018-09-07 17:17:08 -0400

    done with first version

--- test/test_convolve.cpp
+++ test/test_convolve.cpp
@@ -97,18 +97,18 @@ int main()
 	// test applyThreshold on a vector
 	//WTools::applyThreshold(int N, complex<double>* wt, string thresholdRule, int p, complex<double>* thresholdVector, complex<double>* output, double* ratioThresholded)
 	
-	// p = 3
-	int ssdim= 512/pow(2,3);
-	WTools::fwt(512, testvec, ssdim, mey_util, mey_vtil, w);
+	//// p = 3
+	//int ssdim= 512/pow(2,3);
+	//WTools::fwt(512, testvec, ssdim, mey_util, mey_vtil, w);
 
-	double thresholdVector[4] = {1, 1, 0.1, 10};
-	double ratioThresholded[4];
-	complex<double> output[512];
-	WTools::applyThreshold(512, w, "soft", 3, thresholdVector, output, ratioThresholded);
+	//double thresholdVector[4] = {1, 1, 0.1, 10};
+	//double ratioThresholded[4];
+	//complex<double> output[512];
+	//WTools::applyThreshold(512, w, "soft", 3, thresholdVector, output, ratioThresholded);
 
-	WTools::writeReal(512, output, "thresholded");
+	//WTools::writeReal(512, output, "thresholded");
 
-	complex<double>* Mat[4][5];
+	//complex<double>* Mat[4][5];
 	// can fold into a vector or arbitrary length
 	//WTools::fold(4, A, Mat[0][]);
 	//WTools::testMat(Mat, 4, 5);
@@ -139,7 +139,7 @@ int main()
 	//WTools::readReal(n, imp, "imp");
 	//WTools::fft(n, signal, fsignal); 
 	//WTools::fft(n, imp, fimp); 
-	//WTools::fWienDec(n, fsignal, fimp, 11.2, 1, fdeconv, mult);
+	////WTools::fWienDec(n, fsignal, fimp, 11.2, 1, fdeconv, mult);
 	//WTools::writeComplex(n, fdeconv, "fdeconv");
 	//for(int i=0; i<n; i++)
 	//	fdeconvIm[i] = imag(fdeconv[i]) ;
@@ -149,7 +149,82 @@ int main()
 	//WTools::writeReal(n, deconv, "deconv");
 	// scaled wiener deconvolution works!!
 
+//complex<double> WTools::wienForwd(int N, complex<double>* fSignal, complex<double>* fImpulse, complex<double>* basisMatrix, int p, double noiseSd, double* scaling, double* rho, string thresholdRule, complex<double>* wOutput, double* ratioThresholded)
 
+	// deconvolution test
+	int n = 1024;
+	// get the signal and impulse response
+	complex<double> signal[n];
+	complex<double> imp[n];
+	complex<double> fSignal[n];
+	complex<double> fImp[n];
+	WTools::readReal(n, signal, "signal");
+	WTools::readReal(n, imp, "imp");
+	// compute the fourier transform
+	WTools::fft(n, signal, fSignal); 
+	WTools::fft(n, imp, fImp); 
+
+	// set up the wavelet filters
+	complex<double> u_filt[n];
+	complex<double> v_filt[n];
+	complex<double> util_filt[n];
+	complex<double> vtil_filt[n];
+	
+	// generate the filters
+	WTools::filt(n, "d10", u_filt, v_filt, util_filt, vtil_filt);
+
+	// generate the wavelet basis matrix
+	// for p-th stage wavelet based deconvolution
+	int p = 4;
+	complex<double> basisMat[n*(p+1)];
+	WTools::getBasisMatrix(n, u_filt, v_filt, p, basisMat);
+	
+	// ready for deconvolution /////////
+	//
+	//  placeholder for deconvolved wavelet transform
+	complex<double> wOutput[n]; 
+
+	// placeholder for ratio of wavelet coeffients that are thresholded, level-dependent
+	double ratioThresholded[p+1];
+
+	// set up other parameters
+	string thresholdRule = "soft";
+	
+	// standard deviation of noise
+	double noiseSd = 11.2;
+
+	// level-dependent scaling parameters
+	// optimum value to be determined via Newton's method
+	// so that
+	// scaling[j] = ratioThresholded[j]
+	// should be between 0 and 1
+	double scaling[p+1];
+	// setting them just to see if it works
+	for(int i=0; i<p+1; i++)
+		scaling[i] = 0.5;
+
+	// level-dependent wavelet shrinkage parameter
+	// increase them to threshold more, at each level
+	// default is 1 for each j
+	double rho[p+1];
+	for(int i=0; i<p+1; i++)
+		rho[i] = 1;
+
+	
+	// do the actual deconvolution
+	WTools::wienForwd(n,fSignal, fImp,  basisMat, p,  noiseSd, scaling, rho, thresholdRule, wOutput, ratioThresholded);
+
+	// find the inverse wavelet transform
+	complex<double> forwdDeconv[n];
+
+	// stopping dimension for p-th stage inverse wavelet transform
+	int stoppingDim = n/pow(2,p);
+
+	// actual inverse wavelet transform
+	WTools::ifwt(n, wOutput, stoppingDim, u_filt, v_filt, forwdDeconv);
+
+	// write to a file
+	WTools::writeReal(n, forwdDeconv, "forwdDeconv");
 		
 	return 0;
 }
